6a2085bbd76fe4f9ee19545154911aeadd2fbded
Richard Harvey <richard@squarecows.com>
2012-03-21 10:00:04 +0000

    Install services in order and shut them down pending configuration

--- scripts/pre.d/00-install-deps.sh
+++ scripts/pre.d/00-install-deps.sh
@@ -3,4 +3,21 @@
 # Sample file that installs your dependancies on launch
 
 apt-get update
-apt-get install -y apache2 php5 php5-myql mysql-server nginx
+apt-get install -y apache2 php5 php5-mysql php-apc php5-memcached
+
+## Stop apache from running so we can configure it
+
+/etc/init.d/apache2 stop
+
+## Install nginx for our caching layer
+
+apt-get install -y nginx
+
+## Stop Nginx ready for configuration
+
+/etc/init.d/nginx stop
+
+## Install MySQL server
+
+apt-get install -y mysql-server
+
